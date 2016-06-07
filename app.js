02e3d05f6a107c7ffbd5e37987657eb6429831d1
robrotheram <robrotheram@gmail.com>
2016-06-07 16:05:56 +0100

    initial code

---
+++ app.js
@@ -0,0 +1,221 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose    = require('mongoose');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+
+var User   = require('./models/users'); // get our mongoose model
+var Server   = require('./models/servers'); // get our mongoose model
+var app = express();
+
+mongoose.connect('mongodb://robrotheram:mallard@ds011963.mlab.com:11963/api_example');
+const MongoClient = require('mongodb').MongoClient
+var db;
+MongoClient.connect('mongodb://robrotheram:mallard@ds011963.mlab.com:11963/api_example', function(err, database) {
+  if(!err){
+    console.log("DB link establised");
+    db = database;
+  }
+})
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('superSecret', 'bob'); // secret variable
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({
+    name: 'Nick Cerminara',
+    password: 'password',
+    admin: true
+  });
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+app.get('/setupserver', function(req, res) {
+
+  // create a sample user
+  var ser = new Server({
+    name: "test_server",
+    ipaddress: "127.0.0.1",
+    authPublic: "public",
+    authSecret: "secret",
+  });
+  ser.save(function(err) {
+    if (err) throw err;
+
+    console.log('Server saved successfully');
+    res.json({ success: true });
+  });
+});
+
+// ---------------------------------------------------------
+// get an instance of the router for api routes
+// ---------------------------------------------------------
+var apiRoutes = express.Router();
+
+// ---------------------------------------------------------
+// authentication (no middleware necessary since this isnt authenticated)
+// ---------------------------------------------------------
+// http://localhost:8080/api/authenticate
+apiRoutes.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresIn: 600 // expires in 24 hours
+        });
+
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
+
+// ---------------------------------------------------------
+// route middleware to authenticate and check token
+// ---------------------------------------------------------
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+
+  }
+
+});
+
+// ---------------------------------------------------------
+// authenticated routes
+// ---------------------------------------------------------
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+apiRoutes.get('/check', function(req, res) {
+  res.json({"data":req.decoded._doc.name});
+});
+
+app.use('/api', apiRoutes);
+
+app.get('/test', function(req, res, next) {
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.post('/server', function(req, res) {
+  console.log(req.body);
+  var coll = db.collection('logs');
+  coll.insertOne(req.body);
+
+
+
+  res.json(req.body);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
